abf1998a7d351ce6edfc25d0cb4794f4f2705014
Denis Roussel <denis.roussel@gmail.com>
2012-06-18 09:28:22 +0200

    new GA code

--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,5 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA–27319861–2']);
+_gaq.push(['_setAccount', 'UA–32716572–1']);
 _gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
