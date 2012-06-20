77f8e8af85ab930f52a0bc5b2b0dd0ac85a5ed5c
Denis Roussel <denis.roussel@gmail.com>
2012-06-20 10:35:38 +0200

    analytics.js updated

--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,6 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32716572-1']);
+_gaq.push(['_setDomainName', 'crew-cr.org']);
 _gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
